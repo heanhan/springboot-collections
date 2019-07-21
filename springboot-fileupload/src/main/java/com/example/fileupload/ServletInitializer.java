6318ecce312f1f1228b51319b5cde20c542e0dfe
heanhan <1763124707@qq.com>
2019-07-21 10:43:49 +0800

    springboot-collecons:在服务springboot-fileupload的添加主类的初始化

---
+++ springboot-fileupload/src/main/java/com/example/fileupload/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.fileupload;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootFileuploadApplication.class);
+    }
+
+}
