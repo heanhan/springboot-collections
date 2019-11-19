9f3d26f6fc0d047b3c6fd56b6c7d75f0976e54fb
1763124707@qq.com <1763124707@qq.com>
2019-11-19 20:50:41 +0800

    springboot-collections:添加springboot使用webflux。作为前端控制器的使用

---
+++ springboot-webflux/src/main/java/com/example/webflux/SpringbootWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebfluxApplication.class, args);
+    }
+
+}
