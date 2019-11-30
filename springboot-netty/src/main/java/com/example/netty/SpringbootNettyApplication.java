abd3ba0c47c0269f0c67cf5fa5e0af633739e4d2
heanhan <1763124707@qq.com>
2019-11-30 16:38:56 +0800

    springboot-collections:添加使用springboot-netty的服务模块。。。。。。

---
+++ springboot-netty/src/main/java/com/example/netty/SpringbootNettyApplication.java
@@ -0,0 +1,13 @@
+package com.example.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootNettyApplication.class, args);
+    }
+
+}
