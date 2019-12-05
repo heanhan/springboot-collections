f33c8b238e38ec3a133d8ddb6198ac04a289ed14
heanhan <1763124707@qq.com>
2019-12-05 11:23:05 +0800

    springboot-collections:添加使用springboot-netty的服务模块。。。。。。

--- springboot-office/src/main/java/com/example/office/controller/MyController.java
+++ springboot-office/src/main/java/com/example/office/controller/MyController.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.IOUtils;
 import org.jodconverter.DocumentConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.ServletOutputStream;
@@ -29,19 +30,22 @@ public class MyController {
     private HttpServletResponse response;
 
     @RequestMapping("toPdfFile")
-    public String toPdfFile() {
+    public String toPdfFile() throws Exception{
+        //
         //com/example/office/controller/信息系统安全等级保护备案表.doc
-        File file = new File("D:\\work_contents\\2019year\\2019合规项目\\合规总结一些表库以及业务关系.docx");//需要转换的文件
+//        File file = new File("D:\\work_contents\\2019year\\2019合规项目\\合规总结一些表库以及业务关系.docx");//需要转换的文件
+        File file = new File("usr"+File.separator+"local"+File.separator+"uploadfile"+File.separator+"中国人寿部署文档.docx");
         try {
-            File newFile = new File("D:/obj-pdf");//转换之后文件生成的地址
+//            File newFile = ResourceUtils.getFile("classpath:file/");//转换之后文件生成的地址
+            File newFile = new File("usr"+File.separator+"local"+File.separator+"uploadfile");//转换之后文件生成的地址
             if (!newFile.exists()) {
                 newFile.mkdirs();
             }
             //文件转化
-            converter.convert(file).to(new File("D:/obj-pdf/hello.pdf")).execute();
+            converter.convert(file).to(new File("usr"+File.separator+"local"+File.separator+"uploadfile"+File.separator+"hello1.pdf")).execute();
             //使用response,将pdf文件以流的方式发送的前段
             ServletOutputStream outputStream = response.getOutputStream();
-            InputStream in = new FileInputStream(new File("D:/obj-pdf/hello.pdf"));// 读取文件
+            InputStream in = new FileInputStream(new File("usr"+File.separator+"local"+File.separator+"uploadfile"+File.separator+"hello1.pdf"));// 读取文件
             // copy文件
             int i = IOUtils.copy(in, outputStream);
             System.out.println(i);
