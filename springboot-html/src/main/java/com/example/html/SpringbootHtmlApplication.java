852d9bada320cbe7cda13be63ca5d475e52e6d57
zhaojh0912 <1763124707@qq.com>
2020-04-12 11:45:21 +0800

    2020-04-12: 修改springboot-collection的模块，重构springboot-html模块，

--- springboot-html/src/main/java/com/example/html/SpringbootHtmlApplication.java
+++ springboot-html/src/main/java/com/example/html/SpringbootHtmlApplication.java
@@ -3,6 +3,9 @@ package com.example.html;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zhaojh0912
+ */
 @SpringBootApplication
 public class SpringbootHtmlApplication {
 
